11f115abe14a72a3cf21067aa917d6fc94cc8293
snowooden <snowoooden@naver.com>
2023-07-28 19:28:20 +0900

    초기화

--- src/App.js
+++ src/App.js
@@ -1,63 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-const App = () => {
-  const [showContent, setShowContent] = useState(false);
-  const [showText, setShowText] = useState(false);
-  const [buttonClicked, setButtonClicked] = useState(false);
-
-  useEffect(() => {
-    const blackScreenTimer = setTimeout(() => {
-      setShowText(true);
-    }, 3000);
-
-    // const textTimer = setTimeout(() => {
-    //   setShowContent(true);
-    // }, 8000);
-
-    return () => {
-      clearTimeout(blackScreenTimer);
-      // clearTimeout(textTimer);
-    };
-  }, []);
-
-  const handleButtonClick = () => {
-    setButtonClicked(true);
-    setShowContent(true);
-  };
-
+function App() {
   return (
-    <div className="app">
-      {!showContent ? (
-        <div className="black-screen">
-          {showText && (
-            <div className="centered-text">
-              <p className="white-text">
-                현재 보시는 화면은 시각장애인이 보는 화면의 모습입니다.
-              </p>
-              <p className="white-text">웹 페이지를 이해할 수 있도록 코드를 수정하여 웹 접근성을 높여주세요!</p>
-              {!buttonClicked && (
-                <button className="start-button" onClick={handleButtonClick}>
-                  시작하기
-                </button>
-              )}
-            </div>
-          )}
-        </div>
-      ) : (
-        <div className="split-screen">
-          <div className="left-screen"></div>
-          <div className="right-screen">
-            <pre>
-              <code className="source-code">
-                {`// Your source code goes here`}
-              </code>
-            </pre>
-          </div>
-        </div>
-      )}
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
