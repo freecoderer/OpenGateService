76fdcb5f8b659c090f6b45b0e245f36d1d7d69a9
snowooden <snowoooden@naver.com>
2023-07-28 16:40:30 +0900

    첫 화면 멘트 추가

--- src/App.js
+++ src/App.js
@@ -3,19 +3,36 @@ import './App.css';
 
 const App = () => {
   const [showContent, setShowContent] = useState(false);
+  const [showText, setShowText] = useState(false);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowContent(true);
+    const blackScreenTimer = setTimeout(() => {
+      setShowText(true);
     }, 3000);
 
-    return () => clearTimeout(timer);
+    const textTimer = setTimeout(() => {
+      setShowContent(true);
+    }, 8000);
+
+    return () => {
+      clearTimeout(blackScreenTimer);
+      clearTimeout(textTimer);
+    };
   }, []);
 
   return (
     <div className="app">
       {!showContent ? (
-        <div className="black-screen"></div>
+        <div className="black-screen">
+          {showText && (
+            <div className="centered-text">
+              <p className="white-text">
+                현재 보시는 화면은 시각장애인이 보는 화면의 모습입니다.
+              </p>
+              <p className="white-text">웹 페이지를 이해할 수 있도록 코드를 수정하여 웹 접근성을 높여주세요!</p>
+            </div>
+          )}
+        </div>
       ) : (
         <div className="split-screen">
           <div className="left-screen"></div>
