5b8c6682babfe71bbb7e87708335ffe0205c2ed1
snowooden <snowoooden@naver.com>
2023-07-28 17:21:44 +0900

    시작하기 버튼 추가

--- src/App.js
+++ src/App.js
@@ -4,21 +4,27 @@ import './App.css';
 const App = () => {
   const [showContent, setShowContent] = useState(false);
   const [showText, setShowText] = useState(false);
+  const [buttonClicked, setButtonClicked] = useState(false);
 
   useEffect(() => {
     const blackScreenTimer = setTimeout(() => {
       setShowText(true);
     }, 3000);
 
-    const textTimer = setTimeout(() => {
-      setShowContent(true);
-    }, 8000);
+    // const textTimer = setTimeout(() => {
+    //   setShowContent(true);
+    // }, 8000);
 
     return () => {
       clearTimeout(blackScreenTimer);
-      clearTimeout(textTimer);
+      // clearTimeout(textTimer);
     };
   }, []);
+
+  const handleButtonClick = () => {
+    setButtonClicked(true);
+    setShowContent(true);
+  };
 
   return (
     <div className="app">
@@ -30,6 +36,11 @@ const App = () => {
                 현재 보시는 화면은 시각장애인이 보는 화면의 모습입니다.
               </p>
               <p className="white-text">웹 페이지를 이해할 수 있도록 코드를 수정하여 웹 접근성을 높여주세요!</p>
+              {!buttonClicked && (
+                <button className="start-button" onClick={handleButtonClick}>
+                  시작하기
+                </button>
+              )}
             </div>
           )}
         </div>
