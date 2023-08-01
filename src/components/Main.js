a5d86219f92df04c6cad69c6c39904eb14c62b69
snowooden <snowoooden@naver.com>
2023-08-01 18:24:20 +0900

    첫화면 디자인 완료

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,11 +7,10 @@ export default function Main() {
     return (
         <div className='main'>
             <div className='container'>
-                <h1>접근성 테스트</h1>
-                <br />
+                <h2>웹 접근성 테스트</h2>                                
                 <h3>초보 개발자들을 위한 웹 접근성 레벨테스트</h3>
                 <div>
-                    <img src="img/mainlogo.png" alt="" className='logo'></img>
+                    <img src="img/cutelion2.png" alt="" className='logo'></img>
                 </div>
                 <div>
                     <a href="/quiz" class="button btnPush btnbtn">시작하기</a>
